13138c737611277ac20c9eb74769dc098dc017cc
Mike <lvlichaelly@gmail.com>
2015-10-07 20:16:27 -0700

    added explosion effect

--- codacdemy.js
+++ codacdemy.js
@@ -26,4 +26,29 @@ $(document).ready(function () {
         $('div.planet').animate({left: '+=200px'}, 500)
     })
 
+    $('div.planet').click(function () {
+        $(this).effect('explode')
+    })
+
+    (document).keydown(function(key) {
+        switch(parseInt(key.which,10)) {
+  			// Left arrow key pressed
+  			case 37:
+  				$('img').animate({left: "-=10px"}, 'fast');
+  				break;
+  			// Up Arrow Pressed
+  			case 38:
+  				$('img').animate({top: "-=10px"}, 'fast');
+  				break;
+  			// Right Arrow Pressed
+  			case 39:
+  				$('img').animate({left: "+=10px"}, 'fast');
+  				break;
+  			// Down Arrow Pressed
+  			case 40:
+  				$('img').animate({top: "+=10px"}, 'fast');
+  				break;
+    		}
+    	});
+
 })
