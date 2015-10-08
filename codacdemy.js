d833e8433c81083795b96403cca518cbd04ea56f
Mike <lvlichaelly@gmail.com>
2015-10-07 21:31:42 -0700

    example problem on closure

--- codacdemy.js
+++ codacdemy.js
@@ -1,19 +1,20 @@
-$(document).ready(function () {
-    $('#button').click(function () {
-        var toAdd = $('input[name=checkListItem]').val()
-        // item = '<div class="item">' + toAdd + '</div>'
-        $('.list').append('<div class="item">' + toAdd + '</div>')
-
-    })
-
-    $(document).on('click', '.item', function () {
-        this.remove()
-    })
-
-})
-
 
 $(document).ready(function () {
+
+    $('ol').sortable()
+
+   $('#button').click(function () {
+       var toAdd = $('input[name=checkListItem]').val()
+       // item = '<div class="item">' + toAdd + '</div>'
+       $('.list').append('<div class="item">' + toAdd + '</div>')
+
+   })
+
+   $(document).on('click', '.item', function () {
+       this.remove()
+   })
+
+
     $('div.planet').click(function () {
         $(this).fadeOut('fast')
     })
@@ -22,15 +23,15 @@ $(document).ready(function () {
         $(this).toggleClass('red')
     })
 
-    $(document).keydown(function () {
-        $('div.planet').animate({left: '+=200px'}, 500)
-    })
+    // $(document).keydown(function () {
+    //     $('div.planet').animate({left: '+=200px'}, 500)
+    // })
 
     $('div.planet').click(function () {
         $(this).effect('explode')
     })
 
-    (document).keydown(function(key) {
+    $(document).keydown(function(key) {
         switch(parseInt(key.which,10)) {
   			// Left arrow key pressed
   			case 37:
@@ -48,7 +49,9 @@ $(document).ready(function () {
   			case 40:
   				$('img').animate({top: "+=10px"}, 'fast');
   				break;
-    		}
-    	});
+  		  }
+  	});
+
+
 
 })
