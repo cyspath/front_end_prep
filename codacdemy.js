d29bce60cca1234cabdbf2c725387aa4ca6d79f2
Mike <lvlichaelly@gmail.com>
2015-10-07 18:13:54 -0700

    animation for green planet

--- codacdemy.js
+++ codacdemy.js
@@ -17,4 +17,13 @@ $(document).ready(function () {
     $('div.planet').click(function () {
         $(this).fadeOut('fast')
     })
+
+    $('div.planet').hover(function () {
+        $(this).toggleClass('red')
+    })
+
+    $(document).keydown(function () {
+        $('div.planet').animate({left: '+=200px'}, 500)
+    })
+
 })
